564309c390b20eddcff209e636429c7be417a35e
agsjohn <joaovitormds03@gmail.com>
2025-06-13 13:30:10 -0300

    add car brands images, sidebar; update car brands text style

--- func.js
+++ func.js
@@ -73,3 +73,24 @@ function Confirmar() {
     }
     document.getElementById("resposta").innerHTML = text;
 }
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    const menubutton = document.querySelector('.menu-button svg');
+    sidebar.style.display = 'flex';
+    menubutton.style.fill = "none";
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    const menubutton = document.querySelector('.menu-button svg');
+    sidebar.style.display = 'none';
+    menubutton.style.fill = "white";
+}
+
+window.addEventListener('resize', function() {
+    const currentWidth = window.innerWidth;
+    if(currentWidth > 800){
+        hideSidebar();
+    }
+});
